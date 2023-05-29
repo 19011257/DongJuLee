6a921f38336a78709f8814a76f87302bb9be26e4
19011257 <133726926+19011257@users.noreply.github.com>
2023-05-29 22:10:12 +0900

    change

--- HW_12.m
+++ HW_12.m
@@ -2,11 +2,15 @@ semimajor_axis = input('Enter the semimajor axis : ');
 eccentricity = input('Enter the eccentricity : ');
 true_anomaly = input('Enter the true anomaly : ');
 
-disp(solveRangeInPerifocalFrame);
-disp(solveVelocityInPerifocalFrame);
+result_r = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly);
+disp('rangeInPQW : ');
+disp(result_r);
+result_v = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly);
+disp('velocityInPQW =');
+disp(result_v);
 
 function rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
-    true_anomaly_rad = deg2rad(true_anomaly);
+    true_anomaly_rad = true_anomaly*pi/180;
 
     PQW_r = semimajor_axis * (1 - eccentricity^2) / (1 + eccentricity * cos(true_anomaly_rad));
 
@@ -17,18 +21,14 @@ end
 
 
 function velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
-    true_anomaly_rad = deg2rad(true_anomaly);
+    true_anomaly_rad = true_anomaly*pi/180;
 
-    % Gravitational constant (μ) in km^3/s^2
     u = 3.986004418*10^5;
 
-    % Calculate the range (position vector) in the perifocal frame
     p = semimajor_axis * (1 - eccentricity^2);
 
-    % Calculate the magnitude of the velocity in the perifocal frame
     PQW_v = sqrt(u / p);
 
-    % Construct the velocity vector in the perifocal frame
     velocityInPQW = [-PQW_v * sin(true_anomaly_rad);
                      PQW_v * (eccentricity + cos(true_anomaly_rad));
                      0];
